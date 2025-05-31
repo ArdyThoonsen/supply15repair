2334c483eb6be3f623cac747bdf28598856465d1
ArdyThoonsen <thoonsen@bntm.nl>
2025-05-31 19:57:37 +0200

    15repair

---
+++ components/NewsSection.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NewsSection = () => {
+  return <div className="p-4 border rounded">Hier komt nieuws via NewsAPI</div>;
+};
+
+export default NewsSection;
---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-3xl font-bold mb-6">SupplyToday v15</h1>
+      <p className="text-lg text-gray-600">Alles is geladen. Componenten renderen hier live.</p>
+    </div>
+  );
+}
